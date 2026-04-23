6dcbd7f7a7e87d115477f2f5027838d03e4181aa
Nilesh Shah <nilesh.shah@gmail.com>
2026-04-23 15:41:13 +0530

    Finalize image migration: disabled storage, deduplicated Admin app.js, and updated firebase deployment

--- Admin/firebase-config.js
+++ Admin/firebase-config.js
@@ -7,7 +7,7 @@ window.firebaseConfig = {
   authDomain: "prashant-pizza-e86e4.firebaseapp.com",
   databaseURL: "https://prashant-pizza-e86e4-default-rtdb.firebaseio.com",
   projectId: "prashant-pizza-e86e4",
-  storageBucket: "prashant-pizza-e86e4.firebasestorage.app",
+// storageBucket: "prashant-pizza-e86e4.firebasestorage.app",
   messagingSenderId: "857471482885",
   appId: "1:857471482885:web:9eb8bbb90c77c588fbb06c"
 };
