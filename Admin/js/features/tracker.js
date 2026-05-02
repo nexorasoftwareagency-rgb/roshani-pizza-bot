cb0c3054f7196c8d7175279d8549a48100325695
Nilesh Shah <nilesh.shah@gmail.com>
2026-05-02 15:45:03 +0530

    style: change rider location marker to green for better visibility

--- Admin/js/features/tracker.js
+++ Admin/js/features/tracker.js
@@ -99,7 +99,7 @@ function startRiderLocationListener() {
                     // Create new marker
                     const marker = L.marker(pos, {
                         icon: L.icon({
-                            iconUrl: 'https://raw.githubusercontent.com/pointhi/leaflet-color-markers/master/img/marker-icon-2x-blue.png',
+                            iconUrl: 'https://raw.githubusercontent.com/pointhi/leaflet-color-markers/master/img/marker-icon-2x-green.png',
                             shadowUrl: 'https://cdnjs.cloudflare.com/ajax/libs/leaflet/0.7.7/images/marker-shadow.png',
                             iconSize: [25, 41],
                             iconAnchor: [12, 41],
