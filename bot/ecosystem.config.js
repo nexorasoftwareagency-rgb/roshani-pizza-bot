a3a757d9861bebf83177f1fcd328bb608b6b8e2a
Nilesh Shah <nilesh.shah@gmail.com>
2026-05-08 08:48:07 +0530

    Configured multi-site hosting for Admin and Rider

---
+++ bot/ecosystem.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  apps: [{
+    name: 'roshani-bot',
+    script: 'index.js',
+    instances: 1,
+    autorestart: true,
+    watch: false,
+    max_memory_restart: '300M',
+    restart_delay: 10000, 
+    min_uptime: '10s',
+    max_restarts: 10,
+    env: {
+      NODE_ENV: 'production'
+    },
+    exec_mode: 'fork'
+  }]
+};
